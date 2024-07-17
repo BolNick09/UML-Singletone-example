1c34072e8f6db1a18a7218f7b5b65aa708de51b0
BolNick <BolNick09RUS@gmail.com>
2024-07-17 21:31:33 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UML_Singletone_example
+{
+    internal class Program
+    {
+
+        class Singletone
+        {
+            private Singletone() { }
+            private static Singletone single = null;
+
+            public static Singletone getInstance()
+            {
+                if (single == null)
+                    single = new Singletone();
+                return single;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Singletone s1 = Singletone.getInstance();
+            Singletone s2 = Singletone.getInstance();
+
+            Console.WriteLine(s1.GetHashCode()); 
+            Console.WriteLine(s2.GetHashCode()); //выводятся одинаковые хэш коды, s1 и s2 - одна и та же сущность
+
+            //Singletone s3(s1); -- невозможно
+
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("UML Singletone example")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UML Singletone example")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("eff2a476-55b0-49cf-80a2-c129b11650d0")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
